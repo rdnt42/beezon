c9a6f52d4c8141b0e484732d93fc241a49ed6388
marowak <radiant42@yandex.ru>
2023-12-03 14:18:26 +0400

    #1 init couriers tracking

---
+++ couriers_tracking/src/main/kotlin/com/beezon/couriers_tracking/CouriersTrackingApplication.kt
@@ -0,0 +1,11 @@
+package com.beezon.couriers_tracking
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CouriersTrackingApplication
+
+fun main(args: Array<String>) {
+    runApplication<CouriersTrackingApplication>(*args)
+}
